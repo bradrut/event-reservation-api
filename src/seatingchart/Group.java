16fac158210ea4d3cae81f9a798ab3a3f1b9c539
Brad Rutkowski <rutkowski.bradley@gmail.com>
2019-04-03 19:37:15 -0400

    Created method split() to increase cohesion and promote more logical responsibility assignment within the 'update available groups' algorithm.
    Fixed setSmallestDistance() method to account for edge cases.

--- src/seatingchart/Group.java
+++ src/seatingchart/Group.java
@@ -15,19 +15,25 @@ public class Group {
     private final ArrayList<Seat> seatGroup;
     private int smallestDistance;
     private final int rowNum;
+    private int size;
     
     public Group(List<Seat> group, int rowNum){
         this.seatGroup = new ArrayList<>(group);
         this.rowNum = rowNum;
+        size = seatGroup.size();
         setSmallestDistance();
     }
     
     private void setSmallestDistance(){
-        int dist = seatGroup.get(0).getDistance();
-        for(Seat seat : seatGroup){
-            if(seat.getDistance() < dist) dist = seat.getDistance();
+        if(!seatGroup.isEmpty()){
+            int dist = seatGroup.get(0).getDistance();
+            for(Seat seat : seatGroup){
+                if(seat.getDistance() < dist) dist = seat.getDistance();
+            }
+            smallestDistance = dist;
+        }else{
+            smallestDistance = -1;
         }
-        smallestDistance = dist;
     }
     
     int getSmallestDistance(){
@@ -36,6 +42,10 @@ public class Group {
     
     public int getRowNum(){
         return rowNum;
+    }
+    
+    int getSize(){
+        return size;
     }
     
     public ArrayList<Seat> getSeatGroup(){
@@ -57,6 +67,36 @@ public class Group {
             return rowNum <= group.getRowNum();
         }
         return false;
+    }
+    
+    
+    /**
+     * Splits this group into two new groups around the seats that are already
+     * reserved. If all reserved seats are at the beginning or end of the Group,
+     * the first or second group returned, respectively, will be of size 0. 
+     * Note: This method will only work when the reserved seat(s) in this group
+     * are consecutive.
+     * 
+     * @return  An array of groups containing only consecutively available seats.
+     */
+    List<Group> split(){
+        List<Group> newGroups = new ArrayList<>();
+        
+        for(int i=0; i<size; i++){
+            if(!seatGroup.get(i).available()){
+                newGroups.add(new Group(seatGroup.subList(0, i), rowNum));
+                break;
+            }
+        }
+
+        for(int i=size-1; i>=0; i--){
+            if(!seatGroup.get(i).available()){
+                newGroups.add(new Group(seatGroup.subList(i+1, size), rowNum));
+                break;
+            }
+        }
+        
+        return newGroups;
     }
     
 }
