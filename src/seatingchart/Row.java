d4f32089a1a9bd319812bc0f63f9582b22337a87
Brad Rutkowski <rutkowski.bradley@gmail.com>
2019-04-06 10:41:39 -0400

    Removed unused methods reserveSeat() and reserveGroup() as those responsibilities have been moved to the SeatingChart class.

--- src/seatingchart/Row.java
+++ src/seatingchart/Row.java
@@ -3,7 +3,8 @@ package seatingchart;
 import java.util.ArrayList;
 
 /**
- *
+ * Represents a single Row within a SeatingChart which contains a specified number of consecutive seats.
+ * 
  * @author Brad Rutkowski
  */
 public class Row {
@@ -11,6 +12,12 @@ public class Row {
     private final ArrayList<Seat> seats;
     private final int rowNum;
     
+    /**
+     * Constructs a new Row object containing a specified number of seats.
+     * 
+     * @param rowNum    The number of the row that this object will represent within the SeatingChart.
+     * @param numSeats  The number of seats that this row will contain.
+     */
     public Row(int rowNum, int numSeats){
         seats = new ArrayList<>();
         this.rowNum = rowNum;
@@ -19,15 +26,6 @@ public class Row {
     
     public ArrayList<Seat> getSeats(){
         return seats;
-    }
-    
-    String reserveSeat(int colNum){
-        seats.get(colNum-1).setAvailability(false);
-        return seats.get(colNum-1).getLocation();
-    }
-    
-    String reserveGroup(int groupSize){
-        return null;
     }
     
     int getRowNum(){
