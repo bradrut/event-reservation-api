f54349dd77e1817101bd5ff5174dd2ce2fc1280a
Brad Rutkowski <rutkowski.bradley@gmail.com>
2019-04-06 11:01:31 -0400

    Added documentation.

--- src/seatingchart/Group.java
+++ src/seatingchart/Group.java
@@ -4,30 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Represents any grouping of Seat objects. Used primarily to keep track of groups
- * of consecutive available seats within a SeatingChart, but can be used to
- * represent any group of seats within a row.
+ * Represents a group of consecutive Seat objects.
  * 
  * @author Brad Rutkowski
  */
 public class Group {
     
-    private final ArrayList<Seat> seatGroup;
-    private int smallestDistance;
-    private final int rowNum;
-    private int size;
+    private final ArrayList<Seat> seats;    /** The List of Seats within this group. */
+    private final int rowNum;   /** The number of the row in which this Group's seats reside. */
+    private final int size;     /** The number of seats within this group. */
     
-    public Group(List<Seat> group, int rowNum){
-        this.seatGroup = new ArrayList<>(group);
+    /**
+     * The Manhattan distance of the seat with the best location within this group. Used to represent
+     * the Group's overall location quality, since all Seats within this Group should be consecutive.
+     */
+    private int smallestDistance;
+    
+    /**
+     * Constructs a new Group object representing a collection of consecutive Seats.
+     * 
+     * @param seats     A list of consecutive seats which will be stored in this Group.
+     * @param rowNum    The number of the row in which this Group's seats reside.
+     */
+    public Group(List<Seat> seats, int rowNum){
+        this.seats = new ArrayList<>(seats);
         this.rowNum = rowNum;
-        size = seatGroup.size();
+        size = this.seats.size();
         setSmallestDistance();
     }
     
+    
     private void setSmallestDistance(){
-        if(!seatGroup.isEmpty()){
-            int dist = seatGroup.get(0).getDistance();
-            for(Seat seat : seatGroup){
+        if(!seats.isEmpty()){
+            int dist = seats.get(0).getDistance();
+            for(Seat seat : seats){
                 if(seat.getDistance() < dist) dist = seat.getDistance();
             }
             smallestDistance = dist;
@@ -48,17 +58,19 @@ public class Group {
         return size;
     }
     
-    public ArrayList<Seat> getSeatGroup(){
-        return this.seatGroup;
+    public ArrayList<Seat> getSeats(){
+        return this.seats;
     }
     
     /**
-     * Calculates whether this group is better than the given group based on
-     * smallest Manhattan distance and row number.
+     * Calculates whether this group is better than another group based on smallest Manhattan distance 
+     * and row number. Since the Seats in a Group should be consecutive, the Group with the smallest
+     * Manhattan distance is considered better. If both groups have the same smallest distance, the
+     * group with the smaller row number is considered better.
      * 
-     * @param group The group to compare this group to.
-     * @return  Returns true if this group is a better location than the given 
-     *          group, and returns false otherwise.
+     * @param group     The Group to compare this group to.
+     * @return          Returns true if this group holds a better location than the given 
+     *                  group, and returns false otherwise.
      */
     boolean isBetter(Group group){
         if(smallestDistance < group.getSmallestDistance()){
@@ -71,27 +83,25 @@ public class Group {
     
     
     /**
-     * Splits this group into two new groups around the seats that are already
-     * reserved. If all reserved seats are at the beginning or end of the Group,
-     * the first or second group returned, respectively, will be of size 0. 
-     * Note: This method will only work when the reserved seat(s) in this group
-     * are consecutive.
+     * Splits this group into two new groups around the seats that are already reserved within the group. 
+     * If all reserved seats are at the beginning or end of the Group, the first or second group 
+     * returned, respectively, will be of size 0.
      * 
-     * @return  An array of groups containing only consecutively available seats.
+     * @return  Returns an array of groups containing only consecutively available seats.
      */
     List<Group> split(){
         List<Group> newGroups = new ArrayList<>();
         
         for(int i=0; i<size; i++){
-            if(!seatGroup.get(i).available()){
-                newGroups.add(new Group(seatGroup.subList(0, i), rowNum));
+            if(!seats.get(i).available()){
+                newGroups.add(new Group(seats.subList(0, i), rowNum));
                 break;
             }
         }
 
         for(int i=size-1; i>=0; i--){
-            if(!seatGroup.get(i).available()){
-                newGroups.add(new Group(seatGroup.subList(i+1, size), rowNum));
+            if(!seats.get(i).available()){
+                newGroups.add(new Group(seats.subList(i+1, size), rowNum));
                 break;
             }
         }
